8d31f7ba25204c99576149f20c8bb283e4666f8f
Soumwadeep Guha <soumwadeep@gmail.com>
2022-06-21 07:16:08 +0530

    fixed target blank issue

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -14,7 +14,7 @@ const Projects = () => {
           </div>
           <div className="col-md-8">
             <div className="card-body">
-              <a href="https://www.soumwadeepguha.dev/todo"target="_blank"><h5 className="card-title">1. Todo Web App</h5></a>
+              <a href="https://www.soumwadeepguha.dev/todo"target="_blank" rel="noreferrer"><h5 className="card-title">1. Todo Web App</h5></a>
               <p className="card-text">It's A Simple Todo App Which Can Be Used To Store Your Routine And Work Accordingly.</p>
             </div>
           </div>
@@ -28,7 +28,7 @@ const Projects = () => {
           </div>
           <div className="col-md-8">
             <div className="card-body">
-            <a href="https://www.soumwadeepguha.dev/tictactoe"target="_blank"><h5 className="card-title">2. Tic-Tac-Toe Web Game</h5></a>
+            <a href="https://www.soumwadeepguha.dev/tictactoe"target="_blank" rel="noreferrer"><h5 className="card-title">2. Tic-Tac-Toe Web Game</h5></a>
               <p className="card-text">When We Get Bored Of Our Daily Work Loads.We Can Play This Simple Renowned Game With Our Friends To Chill Out And Get Refreshed.</p>
             </div>
           </div>
@@ -42,7 +42,7 @@ const Projects = () => {
           </div>
           <div className="col-md-8">
             <div className="card-body">
-            <a href="https://www.soumwadeepguha.dev/calculator"target="_blank"><h5 className="card-title">3. Scientific Calculator</h5></a>
+            <a href="https://www.soumwadeepguha.dev/calculator"target="_blank" rel="noreferrer"><h5 className="card-title">3. Scientific Calculator</h5></a>
               <p className="card-text">When We Get Caught up With Some Difficult Sums We Can Use This Scientific Calculator To Solve The Problem Instantly.</p>
             </div>
           </div>
@@ -56,7 +56,7 @@ const Projects = () => {
           </div>
           <div className="col-md-8">
             <div className="card-body">
-            <a href="https://www.soumwadeepguha.dev/quiz"target="_blank"><h5 className="card-title">4. Quiz Web App</h5></a>
+            <a href="https://www.soumwadeepguha.dev/quiz"target="_blank" rel="noreferrer"><h5 className="card-title">4. Quiz Web App</h5></a>
               <p className="card-text">We All Know That We Love To Play Quizes Especially Of The Topic Of General Knowledge.So, Here We Can Play Quiz On GK WIth Infinite Questions Which Get's Fetched From API.</p>
             </div>
           </div>
@@ -70,7 +70,7 @@ const Projects = () => {
           </div>
           <div className="col-md-8">
             <div className="card-body">
-            <a href="https://sdgcloudserver.com/"target="_blank"><h5 className="card-title">5. Sdg Cloud Server</h5></a>
+            <a href="https://sdgcloudserver.com/"target="_blank" rel="noreferrer"><h5 className="card-title">5. Sdg Cloud Server</h5></a>
               <p className="card-text">Sdg Cloud Server Is Created To Provide All The Users With The Facility Of Getting Access To All The Latest Documents,Videos,Music,Softwares,Games And More.</p>
             </div>
           </div>
