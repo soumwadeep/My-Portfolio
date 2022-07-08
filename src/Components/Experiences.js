c751e8b50fed86858e79fe1af977fb14a742e87a
Soumwadeep Guha <soumwadeep@gmail.com>
2022-07-09 04:43:45 +0530

    added build

--- src/Components/Experiences.js
+++ src/Components/Experiences.js
@@ -3,51 +3,57 @@ const Experiences = () => {
   return (
     <>
       <div className="row">
-        <div className="col"><h4>Education</h4></div>
-        <div className="col"><h4>Experiences</h4></div>
-        <div className="w-100"></div>
         <div className="col">
-        <p>
-        *<b>2020-2024</b>
-      </p>
-      <p>
-        Persuing <b>B.Tech(Computer Science And Engineering)</b> From Siliguri
-        Institute Of Technology. Have <b>9.49</b> SGPA Till Now.
-      </p>
-      <p>
-        *<b>2017-2018</b>
-      </p>
-      <p>
-        Passed <b>Secondary Examination</b> From Techno India Group Public
-        School With <b>80.8%</b> Marks.
-      </p>
-      <p>
-        *<b>2019-2020</b>
-      </p>
-      <p>
-        Passed <b>Higher Secondary Examination</b> From Techno India Group
-        Public School With <b>72.2%</b> Marks.
-      </p>
+          <h4>Education</h4>
         </div>
         <div className="col">
-        <p>
-        *<b>2020-2024</b>
-      </p>
-      <p>
-        Currently <b>Core Team Member Of Hack Club And Codecademy</b> In SIT. Have <b>Contributed To Different Events</b> Till Now.
-      </p>
-      <p>
-        *<b>2020-2022</b>
-      </p>
-      <p>
-        Was <b>Core Member Of Google Developer Student's Club</b> In SIT And Contributed To <b>Many Events And Projects</b>.
-      </p>
-      <p>
-        *<b>2020-2021</b>
-      </p>
-      <p>
-        Was <b>Campus Ambassador</b> Of Bolt IOT And Skillenza.
-      </p>
+          <h4>Experiences</h4>
+        </div>
+        <div className="w-100"></div>
+        <div className="col">
+          <p>
+            *<b>2020-2024</b>
+          </p>
+          <p>
+            Persuing <b>B.Tech(Computer Science And Engineering)</b> From
+            Siliguri Institute Of Technology. Have <b>9.49</b> SGPA Till Now.
+          </p>
+          <p>
+            *<b>2017-2018</b>
+          </p>
+          <p>
+            Passed <b>Secondary Examination</b> From Techno India Group Public
+            School With <b>80.8%</b> Marks.
+          </p>
+          <p>
+            *<b>2019-2020</b>
+          </p>
+          <p>
+            Passed <b>Higher Secondary Examination</b> From Techno India Group
+            Public School With <b>72.2%</b> Marks.
+          </p>
+        </div>
+        <div className="col">
+          <p>
+            *<b>2020-2024</b>
+          </p>
+          <p>
+            Currently <b>Core Team Member Of Hack Club And Codecademy</b> In
+            SIT. Have <b>Contributed To Different Events</b> Till Now.
+          </p>
+          <p>
+            *<b>2020-2022</b>
+          </p>
+          <p>
+            Was <b>Core Member Of Google Developer Student's Club</b> In SIT And
+            Contributed To <b>Many Events And Projects</b>.
+          </p>
+          <p>
+            *<b>2020-2021</b>
+          </p>
+          <p>
+            Was <b>Campus Ambassador</b> Of Bolt IOT And Skillenza.
+          </p>
         </div>
       </div>
     </>
